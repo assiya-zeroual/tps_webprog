8db19727123b26f60ca142f7a4dafef17a33f018
assiya-zeroual <lascience.doc22@gmail.com>
2025-02-16 20:23:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,196 @@
+< !DOCTYPE
+html >
+< html
+lang = "fr" >
+< head >
+< meta
+charset = "UTF-8" >
+< meta
+name = "viewport"
+content = "width=device-width, initial-scale=1.0" >
+< title > Portfolio - Assiya
+Zeroual < / title >
+< style >
+body
+{
+    font - family: Arial, sans - serif;
+margin: 0;
+padding: 0;
+background - color:  # f4f4f4;
+}
+.container
+{
+    width: 60 %;
+margin: 50
+px
+auto;
+background: white;
+padding: 30
+px;
+box - shadow: 0
+px
+5
+px
+15
+px
+rgba(0, 0, 0, 0.2);
+border - radius: 10
+px;
+}
+header
+{
+    text - align: center;
+padding - bottom: 20
+px;
+border - bottom: 2
+px
+solid  # ddd;
+}
+h1
+{
+    color:  # 333;
+        font - size: 28
+px;
+margin: 0;
+}
+h2
+{
+    color:  # 555;
+        font - size: 22
+px;
+margin - top: 20
+px;
+}
+.section
+{
+    margin - top: 20px;
+padding: 15
+px;
+background:  # f9f9f9;
+border - left: 5
+px
+solid  # 007BFF;
+border - radius: 5
+px;
+}
+ul
+{
+    list - style - type: none;
+padding: 0;
+}
+li
+{
+    padding: 5px 0;
+}
+.profile - img
+{
+    display: block;
+margin: 20
+px
+auto;
+border - radius: 50 %;
+width: 150
+px;
+height: 150
+px;
+object - fit: cover;
+}
+< / style >
+< / head >
+< body >
+< div
+
+
+class ="container" >
+
+< header >
+< img
+src = "C:\Users\hp\Pictures\photo de profil.jpg"
+alt = "Photo de profil"
+
+
+class ="profile-img" >
+
+< h1 > Assiya
+Zeroual < / h1 >
+< p > Élève
+ingénieur
+en
+informatique
+et
+ingénierie
+des
+données - ENSAKH < / p >
+< / header >
+
+< section
+
+
+class ="section contact" >
+
+< h2 > Contact < / h2 >
+< p > Email: lascience.doc22 @ gmail.com < / p >
+< / section >
+
+< section
+
+
+class ="section competences" >
+
+< h2 > Compétences < / h2 >
+< ul >
+< li > Programmation < / li >
+< li > Analyse
+de
+données < / li >
+< li > Développement
+web < / li >
+< / ul >
+< / section >
+
+< section
+
+
+class ="section formation" >
+
+< h2 > Formation < / h2 >
+< ul >
+< li > École
+Nationale
+des
+Sciences
+Appliquées
+de
+Khouribga < / li >
+< li > Spécialisation
+en
+ingénierie
+des
+données < / li >
+< / ul >
+< / section >
+
+< section
+
+
+class ="section experiences" >
+
+< h2 > projet < / h2 >
+< ul >
+< li > traitement
+d
+'image (python) </li>
+< li > Projet
+de
+gestion
+de
+base
+de
+données(gestion
+des
+avions)(sql) < / li >
+< / ul >
+< / section >
+< / div >
+< / body >
+< / html >
